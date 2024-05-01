4c71c9114e03adf8f5af378eab9c7f9a1b589f6a
n.d.parshin <n.d.parshin@tinkoff.ru>
2024-05-01 15:51:00 +0300

    add voxel dog

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,22 +1,41 @@
 import Head from 'next/head'
-import Navbar from '../navbar.js'
+import dynamic from 'next/dynamic'
+import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
+import VoxelDogLoader from '../voxel-dog-loader'
 
-const Main = ({ children, router}) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Nikita Parshin - Homepage</title>
-            </Head>
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
 
-            <Navbar path={router.asPath} />
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Nikita's homepage" />
+        <meta name="author" content="Nikita Parshin" />
+        <meta name="author" content="craftzdog" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Nikita Parshin" />
+        <meta name="og:title" content="Nikita Parshin" />
+        <title>Nikita Parshin - Homepage</title>
+      </Head>
 
-            <Container maxW="container.md" pt={14}>
-                {children}
-            </Container>
-        </Box>
-    )
+      <NavBar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
+
+        {children}
+
+        <Footer />
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
